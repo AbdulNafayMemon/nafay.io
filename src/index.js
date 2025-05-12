e429b7c0fb5d831d4755858d645a1b700c509b0d
Abdul Nafay <abdulnafay1652022@gmail.com>
2025-05-12 16:13:22 +0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TerminalPortfolio from './TerminalPortfolio';
+import './styles.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <TerminalPortfolio />
+    </React.StrictMode>
+);
